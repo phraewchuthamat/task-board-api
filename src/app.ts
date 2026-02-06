f6334ff0bab08faf66d9b37694765e79ca2fde71
Pharew Chuthamat <chuthamatbuaban@gmail.com>
2026-02-06 16:07:30 +0700

    Fix cors app.ts

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,13 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+
+app.use(
+    cors({
+        origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:8080'],
+        credentials: true,
+    })
+)
 app.use(morgan('dev'))
 
 app.use('/auth', authRoutes)
